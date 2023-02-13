a3c4cd861777140ee9848dbc230604022fb7e97d
Sabrina Chen <sabrinaxchen2016@gmail.com>
2023-02-12 22:55:39 -0800

    CARMEN SANDIEGO-Halfway there! Hot on her trail.

---
+++ find_carmen.sql
@@ -0,0 +1,45 @@
+
+-- TEST COMMAND AND SAMPLE OUTPUT
+-- Record your query (or queries, some clues require more than one) below the clue, then comment out the output below it
+-- use two `-` to comment at the start of a line, or highlight the text and press `⌘/` to toggle comments
+-- EXAMPLE: SELECT ALL FROM THE TABLE COUNTRY AND LIMIT IT TO ONE ENTRY
+
+SELECT * FROM COUNTRY LIMIT 1;
+
+--  -[ RECORD 1 ]--+--------------------------
+-- code           | AFG
+-- name           | Afghanistan
+-- continent      | Asia
+-- region         | Southern and Central Asia
+-- surfacearea    | 652090
+-- indepyear      | 1919
+-- population     | 22720000
+-- lifeexpectancy | 45.9
+-- gnp            | 5976.00
+-- gnpold         |
+-- localname      | Afganistan/Afqanestan
+-- governmentform | Islamic Emirate
+-- headofstate    | Mohammad Omar
+-- capital        | 1
+-- code2          | AF
+
+
+-- Clue #1: We recently got word that someone fitting Carmen Sandiego's description has been traveling through Southern Europe. She's most likely traveling someplace where she won't be noticed, so find the least populated country in Southern Europe, and we'll start looking for her there.
+SELECT name, code FROM country where region='Southern Europe' ORDER BY population DESC LIMIT 1;
+
+
+-- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
+ SELECT language FROM countrylanguage WHERE countrycode='ITA' AND isofficial=TRUE;
+
+
+
+-- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
+SELECT language, percentage, countrycode FROM countrylanguage WHERE language='Italian' AND percentage=100;
+
+
+-- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
+
+SELECT countrycode, name FROM city WHERE countrycode='SMR';
+
+
+
